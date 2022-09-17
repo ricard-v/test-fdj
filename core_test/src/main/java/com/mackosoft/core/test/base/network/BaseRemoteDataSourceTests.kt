6e09cc46db629ce05ab00826b54a09aa72eec2ee
Vincent RICARD <mackovich.fr@gmail.com>
2022-09-17 18:06:40 +0200

    [Home Page] Added unit tests for Remote Data Source.

--- core_test/src/main/java/com/mackosoft/core/test/base/network/BaseRemoteDataSourceTests.kt
+++ core_test/src/main/java/com/mackosoft/core/test/base/network/BaseRemoteDataSourceTests.kt
@@ -1,10 +1,38 @@
 package com.mackosoft.core.test.base.network
 
+import android.annotation.SuppressLint
+import androidx.annotation.CallSuper
+import com.mackosoft.core.base.datasource.BaseRemoteDataSource
+import com.mackosoft.core.base.datasource.api.RetrofitClient
 import com.mackosoft.core.test.base.BaseTests
+import io.mockk.every
+import io.mockk.mockkObject
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 
-abstract class BaseRemoteDataSourceTests : BaseTests() {
+abstract class BaseRemoteDataSourceTests<RemoteDataSource : BaseRemoteDataSource> : BaseTests() {
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun init() {
+            mockkObject(RetrofitClient)
+        }
+    }
 
     @get:Rule
-    protected val mockWebServerRule = MockWebServerRule()
+    val mockWebServerRule = MockWebServerRule()
+
+    protected abstract val remoteDataSource: RemoteDataSource
+
+    @SuppressLint("VisibleForTests")
+    @Before
+    @CallSuper
+    open fun setupTest() {
+        // Retrofit
+        every {
+            RetrofitClient.apiBaseUrl
+        } returns mockWebServerRule.server.url("/").toString()
+    }
 }
--- core_test/src/main/java/com/mackosoft/core/test/base/network/MockWebServerExtensions.kt
+++ core_test/src/main/java/com/mackosoft/core/test/base/network/MockWebServerExtensions.kt
@@ -7,19 +7,5 @@ fun MockWebServer.setResponseAndEnqueue(response: String) {
     enqueue(MockResponse().setBody(response))
 }
 
-fun MockWebServer.assertPathMatch(expectedPath: String) {
-    val requestPath = takeRequest().path
-    assert(requestPath == expectedPath) {
-        "Expected to have a <$expectedPath> but instead got <$requestPath>"
-    }
-}
-
-fun MockWebServer.assertPathMatch(expectedPath: String, lazyMessage: (String?) -> String) {
-    val result = takeRequest().path
-    assert(result == expectedPath) {
-        lazyMessage.invoke(result)
-    }
-}
-
 // constant port : https://stackoverflow.com/questions/37965558/okhttp-mockwebserver-fails-to-accept-connections-when-restarted#comment109123321_37965558
 fun MockWebServer.startForMultipleTests() = start(8008)
