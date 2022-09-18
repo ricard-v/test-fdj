b7923e370dd2920ab0fa300c3619b5d98c197c7c
Vincent RICARD <mackovich.fr@gmail.com>
2022-09-18 11:27:07 +0200

    [Team Details] Added basic fragment and added it to nav_graph.

---
+++ feature_team_details/src/main/java/com/mackosoft/features/teamdetails/view/TeamDetailsFragment.kt
@@ -0,0 +1,12 @@
+package com.mackosoft.features.teamdetails.view
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.mackosoft.features.teamdetails.R
+import com.mackosoft.features.teamdetails.databinding.FragmentTeamDetailsBinding
+
+class TeamDetailsFragment : Fragment(R.layout.fragment_team_details) {
+
+    private val binding: FragmentTeamDetailsBinding by viewBinding()
+
+}
